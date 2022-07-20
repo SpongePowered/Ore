5dbb503d842a2b2d322bed13143cca8aa7133333
Felix Kleinekathöfer <felix@felixoi.com>
2022-07-20 16:11:39 +0200

    Updated some frontend deps and removed/added a sponsor (#1065)
    * Updated some frontend deps and removed/added a sponsor
    
    * sbt Updates

--- project/frontendDependencies.scala
+++ project/frontendDependencies.scala
@@ -11,14 +11,14 @@ object NPMDeps {
   val lodash      = "lodash"       -> "4.17.21"
   val queryString = "query-string" -> "7.1.1"
 
-  val fontAwesome        = "@fortawesome/fontawesome-svg-core"   -> "1.3.0"
-  val fontAwesomeSolid   = "@fortawesome/free-solid-svg-icons"   -> "6.0.0"
-  val fontAwesomeRegular = "@fortawesome/free-regular-svg-icons" -> "6.0.0"
-  val fontAwesomeBrands  = "@fortawesome/free-brands-svg-icons"  -> "6.0.0"
+  val fontAwesome        = "@fortawesome/fontawesome-svg-core"   -> "6.1.1"
+  val fontAwesomeSolid   = "@fortawesome/free-solid-svg-icons"   -> "6.1.1"
+  val fontAwesomeRegular = "@fortawesome/free-regular-svg-icons" -> "6.1.1"
+  val fontAwesomeBrands  = "@fortawesome/free-brands-svg-icons"  -> "6.1.1"
 
-  val babel          = "@babel/core"       -> "7.17.2"
-  val babelLoader    = "babel-loader"      -> "8.2.3"
-  val babelPresetEnv = "@babel/preset-env" -> "7.16.11"
+  val babel          = "@babel/core"       -> "7.18.9"
+  val babelLoader    = "babel-loader"      -> "8.2.5"
+  val babelPresetEnv = "@babel/preset-env" -> "7.18.9"
 
   val webpack               = "4.46.0"
   val webpackDevServer      = "3.11.3"
@@ -33,17 +33,17 @@ object NPMDeps {
   val postCssLoader     = "postcss-loader"                     -> "4.3.0"
   val miniCssExtractor  = "mini-css-extract-plugin"            -> "1.6.2"
   val optimizeCssAssets = "optimize-css-assets-webpack-plugin" -> "6.0.1"
-  val autoprefixer      = "autoprefixer"                       -> "10.4.2"
-  val sass              = "sass"                               -> "1.49.7"
+  val autoprefixer      = "autoprefixer"                       -> "10.4.7"
+  val sass              = "sass"                               -> "1.53.0"
 }
 
 object WebjarsDeps {
 
   val jQuery      = "org.webjars.npm" % "jquery"       % "2.2.4"
-  val fontAwesome = "org.webjars"     % "font-awesome" % "6.0.0"
-  val filesize    = "org.webjars.npm" % "filesize"     % "8.0.7"
-  val moment      = "org.webjars.npm" % "moment"       % "2.29.1"
-  val clipboard   = "org.webjars.npm" % "clipboard"    % "2.0.10"
-  val chartJs     = "org.webjars.npm" % "chart.js"     % "3.7.1"
-  val swaggerUI   = "org.webjars"     % "swagger-ui"   % "4.5.0"
+  val fontAwesome = "org.webjars"     % "font-awesome" % "6.1.1"
+  val filesize    = "org.webjars.npm" % "filesize"     % "9.0.1"
+  val moment      = "org.webjars.npm" % "moment"       % "2.29.4"
+  val clipboard   = "org.webjars.npm" % "clipboard"    % "2.0.11"
+  val chartJs     = "org.webjars.npm" % "chart.js"     % "3.8.0"
+  val swaggerUI   = "org.webjars"     % "swagger-ui"   % "4.11.1"
 }
